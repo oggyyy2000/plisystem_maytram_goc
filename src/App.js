5a0efc7c1151a7f65be91aa5f6cd4eefe8f934b7
oggyyy2000 <tuanbeos47@gmail.com>
2023-06-01 17:17:45 +0700

    handle WS

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import React from "react";
 import { useRoutes } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import routes from "./Routes";
+import WSContextProvider from "./components/context/WSContext";
 
 function App() {
   const tabs = useRoutes(routes); // them va chinh route o file Routes
 
   return (
     <>
-      <Navbar />
-      {tabs}
+      <WSContextProvider>
+        <Navbar />
+        {tabs}
+      </WSContextProvider>
     </>
   );
 }
